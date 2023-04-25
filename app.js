cf669490f02464887781d17c190b826c599a6af4
juanjo1998 <juanjosr98@gmail.com>
2023-04-25 10:44:13 -0500

    deploy first express website

---
+++ app.js
@@ -0,0 +1,28 @@
+
+require('dotenv').config()
+const express = require('express')
+const hbs = require('hbs')
+const port = process.env.PORT
+
+const app = express()
+
+// ? hbs
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + "/views/partials")
+
+// ? static content
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/generic', (req, res) => {
+    res.render("generic")
+})
+
+app.get('/elements', (req, res) => {
+    res.render("elements")
+})
+
+app.listen(port, () => console.log(`Listening on port: ${port}`))
